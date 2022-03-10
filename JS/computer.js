ec780dd7516a2ef395e16ee00c9c1552acbbeefb
Ahmed-Aladdin <a7medalaa36@gmail.com>
2022-03-10 14:57:36 +0200

    Update computer.js

--- JS/computer.js
+++ JS/computer.js
@@ -12,19 +12,18 @@ const playBtn = document.querySelector('.play');
 var starting = false;
 var currentPlayer = 1;
 let winner;
-var re;
+
 playerTurn.textContent = `Player Name : ${player1}`
 // Log cell coordinates when clicked
-
 for (i = 0; i < tableData.length; i++) {
     tableData[i].addEventListener('click', (e) => {
         console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
     });
 };
-var re;
 
 
-// Funtions
+
+///////confirm winner////////
 
 function confirming(player) {
     var confirmassg = confirm(`${player} WINS!! do you want play again .`);
@@ -42,8 +41,8 @@ function confirming(player) {
         localStorage.clear();
     }
 }
+//////generate randum numbers/////////
 function generateRandom(min = 0, max = 6) {
-
     var difference = max - min;
     var rand = Math.random();
     rand = Math.floor(rand * difference);
@@ -51,13 +50,12 @@ function generateRandom(min = 0, max = 6) {
     return rand;
 }
 
+ playBtn.addEventListener('click', (confim) => starting = true);
 
 function changeColor(e) {
     // Get clicked column index
-    // let generatedcolo= generateRandom(0, 6);
     let column = e.target.cellIndex;
     var row = [];
-    playBtn.addEventListener('click', (confim) => starting = true);
     if (starting === true) {
         for (i = 5; i > -1; i--) {
             if (currentPlayer === 1) {
@@ -128,26 +126,21 @@ function changeColorcomputer() {
         }
     }
 }
-
+ /////////////////add color to table////////////////////
 Array.prototype.forEach.call(tableData, (cell) => {
     cell.addEventListener('click', changeColor, true);
-
     cell.addEventListener('click', changeColorcomputer, true);
-   
-
-     
+  
     // Set all slots to white for new game.
     cell.style.backgroundColor = 'white';
 });
-
-function colorMatchCheckhard(one, two, three, four,five) {
-    return (one === two && one === three && one === four && one == five &&one !== 'white' && one !== undefined);
-}
+/////////////////easy check ///////////////////////////////////
 
 
 function colorMatchCheck(one, two, three, four){
     return (one === two && one === three && one === four && one !== 'white' && one !== undefined);
 }
+///////horizontal check/////////
 function checkeasy(){
     for (let row = 0; row < tableRow.length; row++){
         for (let col =0; col < 4; col++){
@@ -157,6 +150,7 @@ function checkeasy(){
            }
         }
     }
+/////////vertical check/////////
     for (let col = 0; col < 7; col++){
         for (let row = 0; row < 3; row++){
             if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row+1].children[col].style.backgroundColor,
@@ -167,24 +161,15 @@ function checkeasy(){
     }
 
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function drawCheck() {
-    let fullSlot = []
-    for (i = 0; i < tableData.length; i++) {
-        if (tableData[i].style.backgroundColor !== 'white') {
-            fullSlot.push(tableData[i]);
-        }
-    }
-    if (fullSlot.length === tableData.length) {
-        return true;
-    }
-}   
-
-
+ 
+//////////////////////////////hard check//////////////////////////////
+function colorMatchCheckhard(one, two, three, four,five) {
+    return (one === two && one === three && one === four && one == five &&one !== 'white' && one !== undefined);
+}
 
 function checkhard(){
-
+   ///////horizontal check/////////
     for (let row = 0; row < tableRow.length; row++) {
         for (let col = 0; col < 3; col++) {
             if (colorMatchCheckhard(tableRow[row].children[col].style.backgroundColor, tableRow[row].children[col + 1].style.backgroundColor,
@@ -193,7 +178,7 @@ function checkhard(){
             }
         }
     }
-
+  /////////vertical check/////////
     for (let col = 0; col < 7; col++) {
         for (let row = 0; row < 2; row++) {
             if (colorMatchCheckhard(tableRow[row].children[col].style.backgroundColor, tableRow[row + 1].children[col].style.backgroundColor,
@@ -202,7 +187,7 @@ function checkhard(){
             };
         }
     }
-
+ ///////////////diagonal check 1/////////////
     for (let col = 0; col < 3; col++) {
         for (let row = 0; row < 2; row++) {
             if (colorMatchCheckhard(tableRow[row].children[col].style.backgroundColor, tableRow[row + 1].children[col + 1].style.backgroundColor,
@@ -211,7 +196,7 @@ function checkhard(){
             }
         }
     }
-
+///////////////diagonal check 1/////////////
     for (let col = 0; col < 3; col++) {
         for (let row = 5; row > 3; row--) {
             if (colorMatchCheckhard(tableRow[row].children[col].style.backgroundColor, tableRow[row - 1].children[col + 1].style.backgroundColor,
@@ -225,4 +210,15 @@ function checkhard(){
 }
 
 
-
+//////////////////////////////draw check/////////////////////////////////////////////////////////////////////
+function drawCheck() {
+    let fullSlot = []
+    for (i = 0; i < tableData.length; i++) {
+        if (tableData[i].style.backgroundColor !== 'white') {
+            fullSlot.push(tableData[i]);
+        }
+    }
+    if (fullSlot.length === tableData.length) {
+        return true;
+    }
+} 
